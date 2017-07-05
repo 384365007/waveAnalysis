2230c5288ac9d01696221303596c957776041c2c
yaokai <384365007@qq.com>
2017-07-05 18:28:56 +0800

    振幅比法计算P波start位置，未出时间

--- waveAnalysis/readsac.py
+++ waveAnalysis/readsac.py
@@ -5,6 +5,34 @@ import obspy
 import pylab
 import os
 
+def SearchForPStart(m_data,dot):
+	idx=0
+	length=len(m_data)
+	ave=sum(m_data)/length
+	M_data=list()
+	for idx in range(0,length):
+		M_data.append(m_data[idx]-ave)
+		if(M_data[idx]<0):
+			M_data[idx]=0-M_data[idx]
+	Ave=sum(M_data)/length
+	frontA=sum(M_data[0:127])
+	frontB=sum(M_data[128:255])
+	dot.append(0)
+	Max=0
+	for idx1 in range(0,(length-256)):
+		A=(frontB+4.5*Ave)/(frontA+4.5*Ave)
+		if((A>=2) and (Max<A)):
+			dot[len(dot)-1]=idx1+128
+			Max=A
+		if(Max!=0 and idx1==dot[len(dot)-1]+128):
+			break
+		if(idx1!=length-256):
+			frontA-=M_data[idx1]
+			frontA+=M_data[idx1+128]
+			frontB-=M_data[idx1+128]
+			frontB+=M_data[idx1+256]
+
+
 class sacfile_wave:  
 	def read(self,sFile):  
 		f=open(sFile,'rb')  
@@ -33,11 +61,20 @@ class sacfile_wave:
 		print "sample",round(delta,2),"second and time length",npts*round(delta,2),"second"
 		print "sensitivity for unit conversion",round(resp,2)
 
-	def draw(self,sImageFile,count):  
-		npts=len(self.m_data)  
-		xd=range(1,npts+1)  
+
+	def draw(self,sImageFile,count):
+		dot=list()
+		cirdot=list()
+		SearchForPStart(self.m_data,dot)
+		npts=len(self.m_data)
+		xd=range(1,npts+1)
 		pylab.figure(count)  
-		pylab.plot(xd,self.m_data,linewidth=0.3)  
+		pylab.plot(xd,self.m_data,linewidth=0.3)
+		for DotIdx in dot:
+			cirdot.append(self.m_data[DotIdx])
+		print dot
+		print cirdot
+		pylab.plot(dot,cirdot,'ob-')
 		pylab.savefig(sImageFile)  
 
 	def exportAsc(self,sAscFile):  
